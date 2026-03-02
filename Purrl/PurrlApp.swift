a6c12e058f55dcce93b04f5a5d6c4288e8e8bcb3
Daniel Jacob Chittoor <djchittoor47@gmail.com>
2026-03-02 13:46:37 +0530

    Initial app scaffolding

---
+++ Purrl/PurrlApp.swift
@@ -0,0 +1,17 @@
+//
+//  PurrlApp.swift
+//  Purrl
+//
+//  Created by Daniel Jacob Chittoor on 02/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PurrlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
